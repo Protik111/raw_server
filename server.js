adbcbc6b226563e6809abb6ff332f228628ac407
Rafiur Rahman Protik <40972692+Protik111@users.noreply.github.com>
2023-05-09 20:05:00 +0600

    students route added

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const http = require("http");
 
+const students = [
+    {
+        name: "Rafiur"
+    },
+    {
+        name: "Protik"
+    }
+]
+
 const server = http.createServer((req, res) => {
     // console.log(req.url);
     if(req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.write(JSON.stringify({ message: "Successful" }))
         res.end();
-    }else {
+    }else if(req.url === '/student') {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.write(JSON.stringify(students))
+        res.end();
+    }
+    
+    else {
         res.writeHead(404, { 'Content-Type': 'application/json' });
         res.write(JSON.stringify({ message: "Resource not found" }));
         res.end();
